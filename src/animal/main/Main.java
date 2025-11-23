19c2ee4eb6a9910a1aa5ecb364598d058929888c
FMX <felipemxavier@hotmail.com.br>
2025-11-23 19:38:08 -0300

    configurações

--- src/animal/main/Main.java
+++ src/animal/main/Main.java
@@ -3,18 +3,18 @@ import animal.pet.Pet;
 
 public class Main {
     public static void main(String[] args) {
-        Pet pet1 = new Pet("Raw", "Cachorro", 3);
-        pet1.exibirInfo();
-        pet1.alimentar();
-        System.out.println("Nome do primeiro pet: " + pet1.nome);
-        Pet pet2 = new Pet("Ráu", "Cachorro", 4);
-        pet2.exibirInfo();
-        System.out.println("Nome do segundo pet: " + pet2.nome);
-        Pet pet3 = new Pet("Rex", "Cachorro", 10);
-        pet3.exibirInfo();
-        System.out.println("Nome do terceiro pet: " + pet3.nome);
-        Pet pet4 = new Pet("Felix", "Gato", 1);
-        pet4.exibirInfo();
-        System.out.println("Nome do quarta pet: " + pet4.nome);
+//        Pet pet1 = new Pet("Raw", "Cachorro", 3);
+//        pet1.exibirInfo();
+//        pet1.alimentar();
+//        System.out.println("Nome do primeiro pet: " + pet1.nome);
+//        Pet pet2 = new Pet("Ráu", "Cachorro", 4);
+//        pet2.exibirInfo();
+//        System.out.println("Nome do segundo pet: " + pet2.nome);
+//        Pet pet3 = new Pet("Rex", "Cachorro", 10);
+//        pet3.exibirInfo();
+//        System.out.println("Nome do terceiro pet: " + pet3.nome);
+//        Pet pet4 = new Pet("Felix", "Gato", 1);
+//        pet4.exibirInfo();
+//        System.out.println("Nome do quarta pet: " + pet4.nome);
     }
 }
--- src/animal/pet/Pet.java
+++ src/animal/pet/Pet.java
@@ -1,20 +1,47 @@
 package animal.pet;
 
 public class Pet {
-    public String nome, especie;
-    public int idade;
+    private String nome;
+    private int idade;
+    public String especie;
     public boolean alimentado;
 
-    public Pet(String nome, String especie, int idade){
+    public Pet(){}
+
+    public Pet(String nome, int idade, String especie, boolean alimentado){
         this.nome = nome;
-        this.especie = especie;
         this.idade = idade;
+        this.especie = especie;
         this.alimentado = false;
     }
+
+    public Pet(String nome, String especie, boolean alimentado){
+        this.nome = nome;
+        this.especie = especie;
+        this.alimentado = false;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+
     public void alimentar(){
         this.alimentado = true;
             System.out.println("Pet alimentado!");
         }
+    }
     public void fazerBarulho() {
         if (especie.equals("Cachorro")) {
             System.out.println("AU AU!");
@@ -27,4 +54,3 @@ public class Pet {
     public void exibirInfo() {
         System.out.println("O nome do animal é " + nome + ", sendo da espécie: " + especie + ", no momento com " + idade + " anos de vida e está " + (alimentado ? "alimentado" : "com fome"));
     }
-}
