68bd8131d62c094d1eafa88cbd8bcb8d00ecf134
Felipe Morales <felipemxavier@hotmail.com.br>
2025-11-24 13:45:41 -0300

    Add Cadastro class for user registration management
    adicionando uma classe para CADASTRAR

--- src/animal/cadastro/Cadastro.java
+++ src/animal/cadastro/Cadastro.java
@@ -1,1 +1,21 @@
+package animal.cadastro;
 
+public class Cadastro {
+  private String email;
+  private int CPF;
+  private int senha;
+
+  public Cadastro(String email, int CPF, int senha){
+    this.email = email;
+    this.CPF = CPF;
+    this.senha = senha;
+  }
+
+  public void seCadastro(){
+    System.out.println("Digite seu email: ");
+    System.out.println("Email: " + this.email);
+  }
+
+  public void cancelarCadastro(){
+    System.out.println("Cadastro cancelado!");
+}
